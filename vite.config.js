491f71bf9a8d442c83ccbbfa74ec2bf8151a1417
MauroDev140 <soymauro56@gmail.com>
2026-04-14 00:04:28 -0400

    fix: vite paths relative for page subdirectories

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', // Enable relative paths for GitHub Pages deploying cleanly 
   server: {
     host: true,  // Expose on local network for multiplayer
     port: 5173,
   },
 });
+
